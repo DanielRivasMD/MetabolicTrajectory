a7fac14f79eb30d8c91cc24e21cdc1bf1ae10aae
Daniel Rivas <danielrivasmd@gmail.com>
2025-10-09 13:22:00 +0300

    add vars config

---
+++ src/config/vars.jl
@@ -0,0 +1,26 @@
+####################################################################################################
+
+module Vars
+using ..Paths
+
+""
+const SIG1R_HT_file = joinpath(
+  Paths.XLSX,
+  "ffl_2023-06-14_11_59_sig1r_transcriptoms_hetero_male_female_macro_OneClickMacroV.2.53.1-slice30mins.mac_1.xlsx",
+)
+
+""
+const SIG1R_WT_file = joinpath(
+  Paths.XLSX,
+  "ffl_2023-06-20_12_14_sig1r_transcriptoms_sigko_wt_male_macro_OneClickMacroV.2.53.1-slice30mins.mac_1.xlsx",
+)
+
+""
+const KO_WT_file = joinpath(
+  Paths.XLSX,
+  "ffl_2023-06-27_11_15_ko_wt_female_macro_OneClickMacroV.2.53.1-slice30mins.mac_1.xlsx",
+)
+
+end
+
+####################################################################################################
