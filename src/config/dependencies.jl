378868cf30f6220f0f1b7ca6545e48383475a40e
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-04 13:58:46 +0200

    add dependencies config

---
+++ src/config/dependencies.jl
@@ -0,0 +1,20 @@
+####################################################################################################
+
+using Clustering
+using Colors
+using DataFrames
+using Dates
+using DelimitedFiles
+using Distances
+using Distributions
+using DynamicAxisWarping
+using FFTW
+using FilePathsBase
+using Parameters
+using Plots
+using Random
+using Statistics
+using TOML
+using XLSX
+
+####################################################################################################
