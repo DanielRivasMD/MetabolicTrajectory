c51c7bf0f1780d28298d88fe4eaf2b5f2188bc5a
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-04 11:02:14 +0200

    update saturated subsample config

--- src/live/saturatedSubsampling/config.jl
+++ src/live/saturatedSubsampling/config.jl
@@ -1,28 +1,6 @@
 ####################################################################################################
 
-using DataFrames
-using Dates
 using Parameters: @with_kw
-
-####################################################################################################
-
-struct ExperimentBundle
-  metadata::DataFrame
-  experiment::DataFrame
-end
-
-####################################################################################################
-
-struct SubSampleID
-  subject::Int
-  ixs::Tuple{Int,Int}
-  time::Tuple{DateTime,DateTime}
-end
-
-struct SubSampleContainer
-  subsamples::Vector{Vector{Float64}}
-  ids::Vector{SubSampleID}
-end
 
 ####################################################################################################
 
@@ -30,8 +8,26 @@ end
   metadata::String
   batches::Vector{String}
   nsamples::Int = 100
-  subsample_len::Float64 = 0.075
+  subsample_len::Float64 = 0.030
   subsample_var::Float64 = 0.005
+  sample::Tuple{Int, Int} = (0, 0)
+end
+
+####################################################################################################
+
+using Parameters: @with_kw
+
+####################################################################################################
+
+using TOML
+using FilePathsBase: basename, splitext, joinpath, dirname, isabspath, isdir, mkpath
+
+####################################################################################################
+
+function loadTparams(path::String)
+  params = TrajectoryParams()
+  sample_cfg = path != "" ? symbolise_keys(TOML.parsefile(path)["sample"]) : Dict()
+  return TrajectoryParams(; merge(struct_to_dict(params), sample_cfg)...)
 end
 
 ####################################################################################################
