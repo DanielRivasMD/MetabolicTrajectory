ec2ab09fd7643f80f3467c7cb35369cfe8040a5e
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-16 14:12:09 +0200

    update args util

--- src/util/args.jl
+++ src/util/args.jl
@@ -9,20 +9,43 @@ const HELP =
 
 ####################################################################################################
 
+macro vinfo(args, msg)
+  return :($args["verbose"] ? @info($msg) : nothing)
+end
+
 function struct_to_dict(x)
-  d = Dict{Symbol,Any}()
-  for name in propertynames(x)
-    d[name] = getfield(x, name)
-  end
-  return d
+  Dict(name => getfield(x, name) for name in propertynames(x))
 end
 
 function symbolise_keys(d::Dict)
-  out = Dict{Symbol,Any}()
-  for (k, v) in d
-    out[Symbol(k)] = v
+  Dict(Symbol(k) => v for (k, v) in d)
+end
+
+####################################################################################################
+
+function pipe_args()
+  desc = HELP * "Pipeline"
+  s = ArgParseSettings(description = desc)
+
+  @add_arg_table s begin
+    "--config"
+    help = "Optional TOML config file for PipeParams"
+    arg_type = String
+
+    "--input"
+    help = "Input directory"
+    arg_type = String
+
+    "--output"
+    help = "Output directory"
+    arg_type = String
+
+    "--verbose", "-v"
+    help = "Enable verbose logging"
+    action = :store_true
   end
-  return out
+
+  return parse_args(s)
 end
 
 ####################################################################################################
