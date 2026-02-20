6dab3bb443f8d6f862ab80ef14953e5346fde748
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-20 16:14:34 +0200

    add sigmoid cumulative signal simulation

--- src/live/signalSimulation/ck1.jl
+++ src/live/signalSimulation/ck1.jl
@@ -26,7 +26,7 @@ using Random
 sigma_params = TrajectoryParams(
   metadata = Vars.SMETA_xlsx,
   batches = [Vars.SIG1R_HT_csv, Vars.SIG1R_WT_csv, Vars.KO_WT_csv],
-  nsamples = 100
+  nsamples = 100,
 )
 
 # Load and split
@@ -55,6 +55,31 @@ struct SignalParams
   freq_range::Tuple{Float64,Float64} # (min Hz, max Hz)
   amp_range::Tuple{Float64,Float64}  # (min, max amplitude)
   phase_range::Tuple{Float64,Float64}# (min, max phase in radians)
+end
+
+function simulate_sigmoid(N; k = 1.0, x0 = N / 2)
+  x = range(1, N, length = N)
+  return 1 ./ (1 .+ exp.(-k .* (x .- x0)))
+end
+
+function repeat_segments(segment::Vector{Float64}, n::Int)
+  @assert n >= 1 "n must be at least 1"
+
+  # Start with the first segment unchanged
+  concatenated = copy(segment)
+  prev_max = maximum(segment)
+
+  # Repeat n-1 more times
+  for _ = 2:n
+    seg_min = minimum(segment)
+    shift_amount = prev_max - seg_min
+    shifted = segment .+ shift_amount
+
+    push!(concatenated, shifted...)
+    prev_max = maximum(shifted)
+  end
+
+  return concatenated
 end
 
 function sine_wave(
@@ -186,7 +211,11 @@ display(plt)
 # Hierarchical clustering
 tree = hclust(cost_matrix; linkage = :ward)
 
-plt = plot_grouped_costmatrix(cost_matrix[tree.order, tree.order], ordered_ids[tree.order], meta)
+plt = plot_grouped_costmatrix(
+  cost_matrix[tree.order, tree.order],
+  ordered_ids[tree.order],
+  meta,
+)
 display(plt)
 
 ####################################################################################################
@@ -262,7 +291,11 @@ display(plt)
 # Hierarchical clustering
 tree = hclust(cost_matrix; linkage = :ward)
 
-plt = plot_grouped_costmatrix(cost_matrix[tree.order, tree.order], ordered_ids[tree.order], meta)
+plt = plot_grouped_costmatrix(
+  cost_matrix[tree.order, tree.order],
+  ordered_ids[tree.order],
+  meta,
+)
 display(plt)
 
 ####################################################################################################
@@ -334,7 +367,91 @@ display(plt)
 # Hierarchical clustering
 tree = hclust(cost_matrix; linkage = :ward)
 
-plt = plot_grouped_costmatrix(cost_matrix[tree.order, tree.order], ordered_ids[tree.order], meta)
+plt = plot_grouped_costmatrix(
+  cost_matrix[tree.order, tree.order],
+  ordered_ids[tree.order],
+  meta,
+)
+display(plt)
+
+####################################################################################################
+
+day_sigmoid1 = day_signal1 .+ (simulate_sigmoid(length(day_signal1), k = 0.05) * maximum(day_signal1) * 5)
+day_sigmoid2 = day_signal2 .+ (simulate_sigmoid(length(day_signal2), k = 0.05) * maximum(day_signal2) * 5)
+day_sigmoid3 = day_signal3 .+ (simulate_sigmoid(length(day_signal3), k = 0.05) * maximum(day_signal3) * 5)
+
+week_sigmoid1 = repeat_segments(day_sigmoid1, 7)
+week_sigmoid2 = repeat_segments(day_sigmoid2, 7)
+week_sigmoid3 = repeat_segments(day_sigmoid3, 7)
+
+# Plot one week
+plt = plot(
+  1:(Int(length(week_sigmoid1) / 7)),
+  week_sigmoid1[1:(Int(length(week_sigmoid1) / 7))],
+  title = "Week Signal 1",
+  legend = false,
+)
+display(plt)
+plt = plot(
+  1:(Int(length(week_sigmoid2) / 7)),
+  week_sigmoid2[1:(Int(length(week_sigmoid1) / 7))],
+  title = "Week Signal 2",
+  legend = false,
+)
+display(plt)
+plt = plot(
+  1:(Int(length(week_sigmoid3) / 7)),
+  week_sigmoid3[1:(Int(length(week_sigmoid1) / 7))],
+  title = "Week Signal 3",
+  legend = false,
+)
+display(plt)
+
+plt = plot(1:length(week_sigmoid1), week_sigmoid1, title = "Week Signal 1", legend = false)
+display(plt)
+plt = plot(1:length(week_sigmoid2), week_sigmoid2, title = "Week Signal 2", legend = false)
+display(plt)
+plt = plot(1:length(week_sigmoid3), week_sigmoid3, title = "Week Signal 3", legend = false)
+display(plt)
+
+s1 = collect_subsamples(week_sigmoid1, fake_times(length(week_sigmoid1)), sigma_params)
+s2 = collect_subsamples(week_sigmoid2, fake_times(length(week_sigmoid2)), sigma_params)
+s3 = collect_subsamples(week_sigmoid3, fake_times(length(week_sigmoid3)), sigma_params)
+
+merged = merge_subsamplecontainers([s1, s2, s3], [1, 2, 3])
+
+order = sortperm(merged.ids; by = id -> (id.subject, id.ixs[1]))
+ordered_subsamples = merged.subsamples[order]
+ordered_ids = merged.ids[order]
+
+N = length(ordered_subsamples)
+if N == 0
+  @warn "No subsamples collected for $var, skipping"
+end
+
+# Compute DTW cost matrix
+cost_matrix = zeros(Float64, N, N)
+
+for i = 1:N
+  for j = i:N
+    cost, _, _ = dtw(ordered_subsamples[i], ordered_subsamples[j], SqEuclidean())
+    norm_cost = cost / mean([length(ordered_subsamples[i]), length(ordered_subsamples[j])])
+    cost_matrix[i, j] = norm_cost
+    cost_matrix[j, i] = norm_cost
+  end
+end
+
+plt = plot_grouped_costmatrix(cost_matrix, ordered_ids, meta)
+display(plt)
+
+# Hierarchical clustering
+tree = hclust(cost_matrix; linkage = :ward)
+
+plt = plot_grouped_costmatrix(
+  cost_matrix[tree.order, tree.order],
+  ordered_ids[tree.order],
+  meta,
+)
 display(plt)
 
 ####################################################################################################
