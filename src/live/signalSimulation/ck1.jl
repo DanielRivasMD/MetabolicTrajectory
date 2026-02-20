73622056212f8765fdf280d130c1dc2a99a8504b
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-20 14:32:57 +0200

    add ascendend cumulative signal simulation chunk 1

--- src/live/signalSimulation/ck1.jl
+++ src/live/signalSimulation/ck1.jl
@@ -191,47 +191,119 @@ display(plt)
 
 ####################################################################################################
 
-day_signal1 = cumsum(day_signal1)
-day_signal2 = cumsum(day_signal2)
-day_signal3 = cumsum(day_signal3)
+day_cumsum1 = cumsum(day_signal1)
+day_cumsum2 = cumsum(day_signal2)
+day_cumsum3 = cumsum(day_signal3)
 
-week_signal1 = repeat(day_signal1, 7)
-week_signal2 = repeat(day_signal2, 7)
-week_signal3 = repeat(day_signal3, 7)
+week_cumsum1 = repeat(day_cumsum1, 7)
+week_cumsum2 = repeat(day_cumsum2, 7)
+week_cumsum3 = repeat(day_cumsum3, 7)
 
 # Plot one week
 plt = plot(
-  1:(Int(length(week_signal1) / 7)),
-  week_signal1[1:(Int(length(week_signal1) / 7))],
+  1:(Int(length(week_cumsum1) / 7)),
+  week_cumsum1[1:(Int(length(week_cumsum1) / 7))],
   title = "Week Signal 1",
   legend = false,
 )
 display(plt)
 plt = plot(
-  1:(Int(length(week_signal2) / 7)),
-  week_signal2[1:(Int(length(week_signal1) / 7))],
+  1:(Int(length(week_cumsum2) / 7)),
+  week_cumsum2[1:(Int(length(week_cumsum1) / 7))],
   title = "Week Signal 2",
   legend = false,
 )
 display(plt)
 plt = plot(
-  1:(Int(length(week_signal3) / 7)),
-  week_signal3[1:(Int(length(week_signal1) / 7))],
+  1:(Int(length(week_cumsum3) / 7)),
+  week_cumsum3[1:(Int(length(week_cumsum1) / 7))],
   title = "Week Signal 3",
   legend = false,
 )
 display(plt)
 
-plt = plot(1:length(week_signal1), week_signal1, title = "Week Signal 1", legend = false)
+plt = plot(1:length(week_cumsum1), week_cumsum1, title = "Week Signal 1", legend = false)
 display(plt)
-plt = plot(1:length(week_signal2), week_signal2, title = "Week Signal 2", legend = false)
+plt = plot(1:length(week_cumsum2), week_cumsum2, title = "Week Signal 2", legend = false)
 display(plt)
-plt = plot(1:length(week_signal3), week_signal3, title = "Week Signal 3", legend = false)
+plt = plot(1:length(week_cumsum3), week_cumsum3, title = "Week Signal 3", legend = false)
 display(plt)
 
-s1 = collect_subsamples(week_signal1, fake_times(length(week_signal1)), sigma_params)
-s2 = collect_subsamples(week_signal2, fake_times(length(week_signal2)), sigma_params)
-s3 = collect_subsamples(week_signal3, fake_times(length(week_signal3)), sigma_params)
+s1 = collect_subsamples(week_cumsum1, fake_times(length(week_cumsum1)), sigma_params)
+s2 = collect_subsamples(week_cumsum2, fake_times(length(week_cumsum2)), sigma_params)
+s3 = collect_subsamples(week_cumsum3, fake_times(length(week_cumsum3)), sigma_params)
+
+merged = merge_subsamplecontainers([s1, s2, s3], [1, 2, 3])
+
+order = sortperm(merged.ids; by = id -> (id.subject, id.ixs[1]))
+ordered_subsamples = merged.subsamples[order]
+ordered_ids = merged.ids[order]
+
+N = length(ordered_subsamples)
+if N == 0
+  @warn "No subsamples collected for $var, skipping"
+end
+
+# Compute DTW cost matrix
+cost_matrix = zeros(Float64, N, N)
+
+for i = 1:N
+  for j = i:N
+    cost, _, _ = dtw(ordered_subsamples[i], ordered_subsamples[j], SqEuclidean())
+    norm_cost = cost / mean([length(ordered_subsamples[i]), length(ordered_subsamples[j])])
+    cost_matrix[i, j] = norm_cost
+    cost_matrix[j, i] = norm_cost
+  end
+end
+
+plt = plot_grouped_costmatrix(cost_matrix, ordered_ids, meta)
+display(plt)
+
+# Hierarchical clustering
+tree = hclust(cost_matrix; linkage = :ward)
+
+plt = plot_grouped_costmatrix(cost_matrix[tree.order, tree.order], ordered_ids[tree.order], meta)
+display(plt)
+
+####################################################################################################
+
+week_accum1 = week_signal1 + collect(1:length(week_signal1)) ./ 10
+week_accum2 = week_signal2 + collect(1:length(week_signal2)) ./ 10
+week_accum3 = week_signal3 + collect(1:length(week_signal3)) ./ 10
+
+# Plot one week
+plt = plot(
+  1:(Int(length(week_accum1) / 7)),
+  week_accum1[1:(Int(length(week_accum1) / 7))],
+  title = "Week Signal 1",
+  legend = false,
+)
+display(plt)
+plt = plot(
+  1:(Int(length(week_accum2) / 7)),
+  week_accum2[1:(Int(length(week_accum1) / 7))],
+  title = "Week Signal 2",
+  legend = false,
+)
+display(plt)
+plt = plot(
+  1:(Int(length(week_accum3) / 7)),
+  week_accum3[1:(Int(length(week_accum1) / 7))],
+  title = "Week Signal 3",
+  legend = false,
+)
+display(plt)
+
+plt = plot(1:length(week_accum1), week_accum1, title = "Week Signal 1", legend = false)
+display(plt)
+plt = plot(1:length(week_accum2), week_accum2, title = "Week Signal 2", legend = false)
+display(plt)
+plt = plot(1:length(week_accum3), week_accum3, title = "Week Signal 3", legend = false)
+display(plt)
+
+s1 = collect_subsamples(week_accum1, fake_times(length(week_accum1)), sigma_params)
+s2 = collect_subsamples(week_accum2, fake_times(length(week_accum2)), sigma_params)
+s3 = collect_subsamples(week_accum3, fake_times(length(week_accum3)), sigma_params)
 
 merged = merge_subsamplecontainers([s1, s2, s3], [1, 2, 3])
 
