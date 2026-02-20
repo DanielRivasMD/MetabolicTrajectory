da1b816d8765163d18627e755dd3fd486330a744
Daniel Rivas <danielrivasmd@gmail.com>
2026-02-20 12:54:22 +0200

    add cumulative signal simulation chunk 1

--- src/live/signalSimulation/ck1.jl
+++ src/live/signalSimulation/ck1.jl
@@ -26,7 +26,7 @@ using Random
 sigma_params = TrajectoryParams(
   metadata = Vars.SMETA_xlsx,
   batches = [Vars.SIG1R_HT_csv, Vars.SIG1R_WT_csv, Vars.KO_WT_csv],
-  nsamples = 1000
+  nsamples = 100
 )
 
 # Load and split
@@ -48,9 +48,6 @@ animal_to_group = Dict(row.Animal => row.Group for row in eachrow(meta))
 subdfs = split_by_animal(bundles)
 
 ####################################################################################################
-
-using Random
-using Plots
 
 struct SignalParams
   duration::Float64                  # total seconds
@@ -164,6 +161,48 @@ display(plt)
 s1 = collect_subsamples(week_signal1, fake_times(length(week_signal1)), sigma_params)
 s2 = collect_subsamples(week_signal2, fake_times(length(week_signal2)), sigma_params)
 s3 = collect_subsamples(week_signal3, fake_times(length(week_signal3)), sigma_params)
+
+merged = merge_subsamplecontainers([s1, s2, s3], [1, 2, 3])
+
+order = sortperm(merged.ids; by = id -> (id.subject, id.ixs[1]))
+ordered_subsamples = merged.subsamples[order]
+ordered_ids = merged.ids[order]
+
+N = length(ordered_subsamples)
+if N == 0
+  @warn "No subsamples collected for $var, skipping"
+end
+
+# Compute DTW cost matrix
+cost_matrix = zeros(Float64, N, N)
+
+for i = 1:N
+  for j = i:N
+    cost, _, _ = dtw(ordered_subsamples[i], ordered_subsamples[j], SqEuclidean())
+    norm_cost = cost / mean([length(ordered_subsamples[i]), length(ordered_subsamples[j])])
+    cost_matrix[i, j] = norm_cost
+    cost_matrix[j, i] = norm_cost
+  end
+end
+
+plt = plot_grouped_costmatrix(cost_matrix, ordered_ids, meta)
+display(plt)
+
+# Hierarchical clustering
+tree = hclust(cost_matrix; linkage = :ward)
+
+plt = plot_grouped_costmatrix(cost_matrix[tree.order, tree.order], ordered_ids[tree.order], meta)
+display(plt)
+
+####################################################################################################
+
+week_cumsum1 = cumsum(week_signal1)
+week_cumsum2 = cumsum(week_signal2)
+week_cumsum3 = cumsum(week_signal3)
+
+s1 = collect_subsamples(week_cumsum1, fake_times(length(week_cumsum1)), sigma_params)
+s2 = collect_subsamples(week_cumsum2, fake_times(length(week_cumsum2)), sigma_params)
+s3 = collect_subsamples(week_cumsum3, fake_times(length(week_cumsum3)), sigma_params)
 
 merged = merge_subsamplecontainers([s1, s2, s3], [1, 2, 3])
 
