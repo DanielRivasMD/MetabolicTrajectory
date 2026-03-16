9109826d8dc018617458c340c8cb8ea6d6a3c824
Daniel Rivas <danielrivasmd@gmail.com>
2026-03-16 16:15:20 +0200

    migrate temp code

--- bin/sigma.jl
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env julia
-using Pkg
-Pkg.activate(joinpath(@__DIR__, ".."))
-
-include(joinpath(@__DIR__, "..", "src", "MetabolicTrajectories.jl"))
-using .MetabolicTrajectories
-
-MetabolicTrajectories.CLI.main(ARGS)
