1016a0e12bbd02665e5a0df9370f8f63c9406d98
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-27 09:36:55 +0300

    add data processing repl

---
+++ src/inter/repl/dprepl.jl
@@ -0,0 +1,35 @@
+####################################################################################################
+
+module DPREPL
+
+####################################################################################################
+
+using Avicenna.Flow: Cache, launch
+using ..DPFlow: flow
+using ..DPCore
+
+####################################################################################################
+
+export run
+
+####################################################################################################
+
+"""
+    run(config_path::String; out_dir=nothing, no_cache=false) -> Result
+
+Run the data ingestion workflow from the REPL
+"""
+function run(config_path::String; out_dir = nothing, no_cache = false)
+  config = Dict{String,Any}("config_path" => config_path)
+  if out_dir !== nothing
+    config["out_dir"] = out_dir
+  end
+  cache = Cache("cache/data_processing", !no_cache)
+  return launch(flow, config; cache = cache)
+end
+
+####################################################################################################
+
+end
+
+####################################################################################################
