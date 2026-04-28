14253f66fda258278a7db00b428fce19f4174d3b
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-28 10:58:58 +0300

    update data processing flow

--- src/flow/dpflow.jl
+++ src/flow/dpflow.jl
@@ -13,19 +13,58 @@ export flow
 
 ####################################################################################################
 
-function dp_stage(config::Dict, prev::Dict)
-  config_path = config["config_path"]
-  params = DPCore.load_dp_params(config_path)
+const flow = Config(
+  "data_processing",
+  [
+    Stage(
+      "01_load_params",
+      (config, _) -> begin
+        return DPCore.load_params(config["config_path"])
+      end,
+      "1.0",
+    ),
+    Stage("02_make_path", (_, prev) -> begin
+      params = prev["01_load_params"]
+      mkpath(params.outdir)
+      return nothing
+    end, "1.0"),
+    Stage(
+      "03_load_experiments",
+      (_, prev) -> begin
+        params = prev["01_load_params"]
+        return DPCore.load_experiments(params)
+      end,
+      "1.0",
+    ),
+    Stage("04_build_meta", (_, prev) -> begin
+      bundles = prev["03_load_experiments"]
+      return DPCore.build_meta(bundles)
+    end, "1.0"),
+    Stage(
+      "05_write_meta",
+      (_, prev) -> begin
+        params = prev["01_load_params"]
+        meta = prev["04_build_meta"]
+        DPCore.writedf(joinpath(params.outdir, params.outmeta), meta; sep = ',')
+        return nothing
 
-  if haskey(config, "out_dir")
-    params = DPCore.DPParams(params.metadata, params.batches, config["out_dir"])
-  end
-  return DPCore.run_dp(params)
-end
-
-####################################################################################################
-
-const flow = Config("data_processing", [Stage("01_file_loading", dp_stage, "1.0")], "1.0")
+      end,
+      "1.0",
+    ),
+    Stage(
+      "06_write_subjects",
+      (_, prev) -> begin
+        params = prev["01_load_params"]
+        bundles = prev["03_load_experiments"]
+        dfs = DPCore.split_by_subject(bundles)
+        DPCore.writedf_dict(params.outdir, dfs; sep = ',')
+        return nothing
+      end,
+      "1.0",
+    ),
+  ],
+  "1.0",
+)
 
 ####################################################################################################
 
