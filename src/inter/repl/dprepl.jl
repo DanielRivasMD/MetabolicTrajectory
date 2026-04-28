dd4d33253cbe89f4e6ab09ce21df07281c83e7c0
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-28 10:59:16 +0300

    update data processing repl

--- src/inter/repl/dprepl.jl
+++ src/inter/repl/dprepl.jl
@@ -15,15 +15,12 @@ export run
 ####################################################################################################
 
 """
-    run(config_path::String; out_dir=nothing, no_cache=false) -> Result
+    run(config_path::String; no_cache=false) -> Result
 
 Run the data processing workflow from the REPL
 """
-function run(config_path::String; out_dir = nothing, no_cache = false)
+function run(config_path::String; no_cache = false)
   config = Dict{String,Any}("config_path" => config_path)
-  if out_dir !== nothing
-    config["out_dir"] = out_dir
-  end
   cache = Cache("cache/data_processing", !no_cache)
   return launch(flow, config; cache = cache)
 end
